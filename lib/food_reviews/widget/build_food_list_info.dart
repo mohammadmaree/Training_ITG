a6a036d47acd92538baa7592573800f9e036c8d2
mmaree <mohammed99.m964@gmail.com>
2022-04-06 15:25:57 +0300

    update

---
+++ lib/food_reviews/widget/build_food_list_info.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BuildFoodListFood extends StatelessWidget {
+  final String name;
+  final String followers;
+  final double screenHeight;
+  final double screenWidth;
+  BuildFoodListFood({
+    required this.screenWidth,
+    required this.screenHeight,
+    required this.name,
+    required this.followers,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
